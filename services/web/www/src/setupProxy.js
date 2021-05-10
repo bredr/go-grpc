36699d638d5cdf891957665ef164e20c5eac6527
Dave Brett <david.brett@tessella.com>
2021-05-10 20:36:40 +0100

    add stub backend

--- services/web/www/src/setupProxy.js
+++ services/web/www/src/setupProxy.js
@@ -1,11 +1,32 @@
 const createProxyMiddleware = require("http-proxy-middleware");
+const STUB = process.env.STUB;
 
-module.exports = function (app) {
-  app.use(
-    "/query",
-    createProxyMiddleware({
-      target: "http://localhost:3080",
-      changeOrigin: true,
-    })
-  );
-};
+if (STUB) {
+  const { buildClientSchema } = require('graphql');
+  const { ApolloServer } = require('apollo-server');
+  const introspectionResult = require('../graphql.schema.json');
+  const schema = buildClientSchema(introspectionResult);
+  const server = new ApolloServer({
+    schema,
+    mocks: true,
+  });
+  module.exports = function (app) {
+    server.listen().then(({ url }) => {
+      console.log(`🚀 Server ready at ${url}`);
+      app.use("/query", createProxyMiddleware({
+        target: url,
+        changeOrigin: true,
+      }))
+    });
+  }
+} else {
+  module.exports = function (app) {
+    app.use(
+      "/query",
+      createProxyMiddleware({
+        target: "http://localhost:3080",
+        changeOrigin: true,
+      })
+    );
+  };
+}
